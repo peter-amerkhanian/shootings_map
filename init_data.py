e656ca5738481d2253249130f7294b47b538b4a8
Peter Amerkhanian <peteamer@gmail.com>
2019-08-08 10:21:35 -0500

    added more documentation

--- init_data.py
+++ init_data.py
@@ -4,13 +4,14 @@ from datetime import datetime
 import pandas as pd
 from utils import states
 
+
 url = "https://docs.google.com/spreadsheets/d/1b9o6uDO18sLxBqPwl_Gh9bnhW-ev_dABH83M5Vb5L8o/htmlview?sle=true#gid=0"
 today = datetime.today()
 
 
-def parse_data(uri):
+def get_data(uri):
     """
-    parse the data from the web
+    HELPER for build(). get the table from Mother Jones
     :param uri: a url of the google spreadsheet
     :return: generate a list of rows in the spreadsheet
     """
@@ -33,23 +34,25 @@ def parse_data(uri):
         temp.append(elem.text)
 
 
-def build():
-    df = pd.DataFrame(list(parse_data(url)))
-    df.at[0, 7] = 'setting'
-    df.columns = df.iloc[0]
-    df = df.drop(0)
-    return df
-
-
-def abbreviation_translate(abbreviation):
-    full = states.get(abbreviation.upper())
+def abbreviation_translate(state):
+    """
+    HELPER for alter_rows(). Replaces abbreviated state names with full name
+    :param state: the state entry, could be complete, could be abrev
+    :return: string of the state in full name form
+    """
+    full = states.get(state.upper())
     if full:
         return full
     else:
-        return abbreviation
+        return state
 
 
-def clean_df(df):
+def correct_types(df):
+    """
+    HELPER for clean(). Changes data types for the columns in the df
+    :param df: the Mother Jones data in df form
+    :return: df object with corrected types
+    """
     df['fatalities'] = df['fatalities'].astype('int')
     df['injured'] = df['injured'].astype('int')
     df['total_victims'] = df['total_victims'].astype('int')
@@ -57,12 +60,47 @@ def clean_df(df):
     df['age_of_shooter'] = df['age_of_shooter'].astype('int')
     df['latitude'] = df['latitude'].astype('float')
     df['longitude'] = df['longitude'].astype('float')
+    return df
+
+
+def alter_rows(df):
+    """
+    HELPER for clean(). Creates state and city out of location row, applies abbreviation_translate.
+    :param df: the Mother Jones data in df form
+    :return: df object with new columns and corrected state column
+    """
     df['city'], df['state'] = df['location'].str.split(", ").str
     df['state'] = [abbreviation_translate(state) for state in df['state']]
     return df
 
 
+def build():
+    """
+    Build the data frame with Mother Jones' data
+    :return: df with raw data
+    """
+    df = pd.DataFrame(list(get_data(url)))
+    df.at[0, 7] = 'setting'
+    df.columns = df.iloc[0]
+    df = df.drop(0)
+    return df
+
+
+def clean(df):
+    """
+    Clean the data
+    :param df: data frame with Mother Jones' data
+    :return: df with cleaned data
+    """
+    df = correct_types(df)
+    df = alter_rows(df)
+    return df
+
+
 def init_data():
-    df = build()
-    df = clean_df(df)
+    """
+    Build and clean the data
+    :return: df with cleaned version of Mother Jones data
+    """
+    df = clean(build())
     return df
